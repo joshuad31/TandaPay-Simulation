4741b6a24ccf4afc00b0a88f58a34f348781219f
RPi Guru <rpiguru@techie.com>
2021-04-05 16:34:57 +0800

    Refactored.

--- utils/tandapay.py
+++ utils/tandapay.py
@@ -93,129 +93,125 @@ class TandaPaySimulator(object):
 
         logger.debug(f'EV1: {self.ev[0]}')
 
+        self.init_user_rec()
+        self.init_sys_rec()
+
+        # Subgroup  # FUNCTION FOR SUBGROUP EXECUTION
+        step1_ev1 = self.ev[0]
+        step2 = self.ev[0] / 5
+        step3 = round(step2 / 2.3333)
+        step4 = step3 * 5
+        step5 = step1_ev1 - step4
+        step6 = step5 / 6
+        step7 = round(step6 / 2)
+        step8 = step7 * 6
+        step9 = step5 - step8
+        step10 = step9 / 7
+        step11 = int(step10 / 2)
+        step12 = step11 * 7
+        step13 = step9 - step12
+        step14 = int(step13 / 4)
+        step15 = step13 % 4
+        if step15 == 1:
+            step3 -= 1
+            step7 += 1
+        elif step15 == 2:
+            step3 -= 1
+            step11 += 1
+        elif step15 == 3:
+            step3 -= 1
+            step14 += 2
+
+        # Assigning number to the group, condition checking for group == 4
+        group_num = 1
+        group_mem_count = 0
+        temp_val_four = step14 * 4
+        offset = 0
+        for i in range(temp_val_four):
+            self.set_orig_subgroup(i + offset, group_num)
+            self.set_remaining_num_orig_subgroup(i + offset, 4)
+            self.set_cur_subgroup(i + offset, group_num)
+            self.set_remaining_num_cur_subgroup(i + offset, 4)
+            group_mem_count += 1
+            if group_mem_count == 4:
+                group_num += 1
+                group_mem_count = 0
+        offset += temp_val_four
+
+        # condition checking for group == 5
+        temp_val_five = step3 * 5
+        for i in range(temp_val_five):
+            self.set_orig_subgroup(i + offset, group_num)
+            self.set_remaining_num_orig_subgroup(i + offset, 5)
+            self.set_cur_subgroup(i + offset, group_num)
+            self.set_remaining_num_cur_subgroup(i + offset, 5)
+            group_mem_count += 1
+            if group_mem_count == 5:
+                group_num += 1
+                group_mem_count = 0
+        offset += temp_val_five
+
+        # condition checking for group == 6
+        temp_val_six = step7 * 6
+        for i in range(temp_val_six):
+            self.set_orig_subgroup(i + offset, group_num)
+            self.set_remaining_num_orig_subgroup(i + offset, 6)
+            self.set_cur_subgroup(i + offset, group_num)
+            self.set_remaining_num_cur_subgroup(i + offset, 6)
+            group_mem_count += 1
+            if group_mem_count == 6:
+                group_num += 1
+                group_mem_count = 0
+        offset += temp_val_six
+
+        # condition checking for group == 7
+        temp_val_seven = step11 * 7
+        for i in range(temp_val_seven):
+            self.set_orig_subgroup(i + offset, group_num)
+            self.set_remaining_num_orig_subgroup(i + offset, 7)
+            self.set_cur_subgroup(i + offset, group_num)
+            self.set_remaining_num_cur_subgroup(i + offset, 7)
+            group_mem_count += 1
+            if group_mem_count == 7:
+                group_num += 1
+                group_mem_count = 0
+
+        checksum = offset + temp_val_seven
+        if checksum != self.ev[0]:
+            raise ValueError(f"Initial group checksum failed: checksum:{checksum} != EV1:{self.ev[0]}")
+        logger.debug({"D": temp_val_four, "A": temp_val_five, "B": temp_val_six, "C": temp_val_seven})
+
+        # setting valid to UsRec5
+        for i in range(self.ev[0]):
+            self.set_subgroup_status(i, 'valid')
+        logger.debug(
+            f'Group4 members: {step14}, Group5 members: {step3}, Group6 members: {step7}, '
+            f'Group7 members: {step11}, Total group: {step14 * 4 + step3 * 5 + step7 * 6 + step11 * 7}')
+
+        # ROLE 1
+        role_ev4 = int(self.ev[0] * self.ev[3])     # EV 4 = Percentage of honest defectors
+        defectors = random.sample(range(self.ev[0]), role_ev4)
+        non_defectors = [i for i in range(self.ev[0]) if i not in defectors]
+        # EV 5 = Percentage of low-morale members
+        role_ev5 = int(self.ev[0] * self.ev[4])
+        low_morale_list = random.sample(non_defectors, role_ev5)
+        for i in range(self.ev[0]):
+            self.set_primary_role(
+                i, 'defector' if i in defectors else 'low-morale' if i in low_morale_list else 'unity-role')
+
+        # ROLE 2
+        # temp_val_four users and pick remaining users randomly to be equal with EV6
+        remaining_pct = int(self.ev[5] * self.ev[0]) - temp_val_four
+        if remaining_pct > 0:
+            rand_dep_user = random.sample(range(temp_val_four, self.ev[0]), remaining_pct)
+        else:
+            rand_dep_user = []
+        for i in range(self.ev[0]):
+            self.set_secondary_role(
+                i, 'dependent' if (i < temp_val_four or i in rand_dep_user) else 'independent')
+
         for self.counter in range(1, count + 1):
             # logger.info(f'Current period is: {self.counter}')
-            if self.counter == 1:
-                self.init_user_rec()
-                self.init_sys_rec()
-                # Subgroup  # FUNCTION FOR SUBGROUP EXECUTION
-                step1_ev1 = self.ev[0]
-                step2 = self.ev[0] / 5
-                step3 = round(step2 / 2.3333)
-                step4 = step3 * 5
-                step5 = step1_ev1 - step4
-                step6 = step5 / 6
-                step7 = round(step6 / 2)
-                step8 = step7 * 6
-                step9 = step5 - step8
-                step10 = step9 / 7
-                step11 = int(step10 / 2)
-                step12 = step11 * 7
-                step13 = step9 - step12
-                step14 = int(step13 / 4)
-                step15 = step13 % 4
-                if step15 == 1:
-                    step3 = step3 - 1
-                    step7 = step7 + 1
-                elif step15 == 2:
-                    step3 = step3 - 1
-                    step11 = step11 + 1
-                elif step15 == 3:
-                    step3 = step3 - 1
-                    step14 = step14 + 2
-
-                # now assigning number to the group, condition checking for group == 4
-                group_num = 1
-                group_mem_count = 0
-                temp_val_four = step14 * 4
-                offset = 0
-                for i in range(temp_val_four):
-                    self.set_orig_subgroup(i + offset, group_num)
-                    self.set_remaining_num_orig_subgroup(i + offset, 4)
-                    self.set_cur_subgroup(i + offset, group_num)
-                    self.set_remaining_num_cur_subgroup(i + offset, 4)
-                    group_mem_count += 1
-                    if group_mem_count == 4:
-                        group_num += 1
-                        group_mem_count = 0
-                offset += temp_val_four
-
-                # condition checking for group == 5
-                temp_val_five = step3 * 5
-                for i in range(temp_val_five):
-                    self.set_orig_subgroup(i + offset, group_num)
-                    self.set_remaining_num_orig_subgroup(i + offset, 5)
-                    self.set_cur_subgroup(i + offset, group_num)
-                    self.set_remaining_num_cur_subgroup(i + offset, 5)
-                    group_mem_count += 1
-                    if group_mem_count == 5:
-                        group_num += 1
-                        group_mem_count = 0
-                offset += temp_val_five
-
-                # condition checking for group == 6
-                temp_val_six = step7 * 6
-                for i in range(temp_val_six):
-                    self.set_orig_subgroup(i + offset, group_num)
-                    self.set_remaining_num_orig_subgroup(i + offset, 6)
-                    self.set_cur_subgroup(i + offset, group_num)
-                    self.set_remaining_num_cur_subgroup(i + offset, 6)
-                    group_mem_count += 1
-                    if group_mem_count == 6:
-                        group_num += 1
-                        group_mem_count = 0
-                offset += temp_val_six
-
-                # condition checking for group == 7
-                temp_val_seven = step11 * 7
-                for i in range(temp_val_seven):
-                    self.set_orig_subgroup(i + offset, group_num)
-                    self.set_remaining_num_orig_subgroup(i + offset, 7)
-                    self.set_cur_subgroup(i + offset, group_num)
-                    self.set_remaining_num_cur_subgroup(i + offset, 7)
-                    group_mem_count += 1
-                    if group_mem_count == 7:
-                        group_num += 1
-                        group_mem_count = 0
-
-                checksum = offset + temp_val_seven
-                if checksum != self.ev[0]:
-                    raise ValueError(f"Initial group checksum failed: checksum:{checksum} != EV1:{self.ev[0]}")
-                logger.debug({"D": temp_val_four, "A": temp_val_five, "B": temp_val_six, "C": temp_val_seven})
-
-                # setting valid to UsRec5
-                for i in range(self.ev[0]):
-                    self.set_subgroup_status(i, 'valid')
-                logger.debug(
-                    f'Group of 4 members: {step14}, 5 members: {step3}, 6 members: {step7}, '
-                    f'7 members: {step11}, Total group: {step14 * 4 + step3 * 5 + step7 * 6 + step11 * 7})')
-
-                # ROLE 1
-                # EV 4 = Percentage of honest defectors
-                role_ev4 = int(self.ev[0] * self.ev[3])
-                defectors = random.sample(range(self.ev[0]), role_ev4)
-                non_defectors = [i for i in range(self.ev[0]) if i not in defectors]
-                # EV 5 = Percentage of low-morale members
-                role_ev5 = int(self.ev[0] * self.ev[4])
-                low_morale_list = random.sample(non_defectors, role_ev5)
-                for i in range(self.ev[0]):
-                    self.set_primary_role(
-                        i, 'defector' if i in defectors else 'low-morale' if i in low_morale_list else 'unity-role')
-
-                # ROLE 2
-                # temp_val_four users and pick remaining users randomly to be equal with EV6
-                remaining_pct = int(self.ev[5] * self.ev[0]) - temp_val_four
-                if remaining_pct > 0:
-                    rand_dep_user = random.sample(range(temp_val_four, self.ev[0]), remaining_pct)
-                else:
-                    rand_dep_user = []
-
-                for i in range(self.ev[0]):
-                    self.set_secondary_role(
-                        i, 'dependent' if (i < temp_val_four or i in rand_dep_user) else 'independent')
-
-                logger.debug('Roles Assigned!')
-
             self.assign_variables()
             if self.counter == 1:
                 self.user_func_1()
@@ -227,10 +223,6 @@ class TandaPaySimulator(object):
 
             self.sys_func_4()
 
-            # Sys Func 4 PATH 1
-            for k in range(1, 20):
-                self.sy_rec_f[k].value = self.sy_rec_p[k].value
-                self.sy_rec_r[k].value = self.sy_rec_p[k].value
             if self.counter == 1:
                 # ___SyFunc5___
                 self.sy_rec_f[9].value = self.sy_rec_f[3].value * self.sy_rec_f[19].value
@@ -239,7 +231,7 @@ class TandaPaySimulator(object):
             self.sys_func_6()
 
             self.sys_func_7()
-            self._checksum(7)
+            self._checksum(7)       # SysFunc7 doesn't perform checksum inside it.
 
             self.sys_func_8()
 
@@ -261,11 +253,10 @@ class TandaPaySimulator(object):
 
                 self._checksum(11)
                 self._checksum_sr1(sy_rec_new_p[1].value, 11)
-
-            if self.counter == count or total == 0:
+            else:
                 self.save_to_excel('user')
                 self.save_to_excel('system')
-                logger.info(f'Complete at period {self.counter}({time.time() - s_time}), logging simulation results')
+                logger.info(f'Complete at period {self.counter}, elapsed: {time.time() - s_time}')
                 percent = round(self.sh_system.cell(3, 5).value / self.ev[0] * 100, 2)
                 inc_premium = round((self.sy_rec_f[19].value / self.sh_system.cell(2, 21).value) * 100, 2)
                 result_file = os.path.join(target_dir, "result.txt")
@@ -312,7 +303,6 @@ class TandaPaySimulator(object):
             self.set_total_payment_specific_user(i, self.ev[0])
             self.set_payable(i, 'yes')
             self.set_defect_count(i, 0)
-        logger.debug('Initial values for UsRec variables set!')
 
     def init_sys_rec(self):
         for i in range(2):
@@ -325,7 +315,6 @@ class TandaPaySimulator(object):
         for i in range(3, 30):
             for k in range(3, 22):
                 self.sh_system.cell(i + 2, k).value = 0
-        logger.debug('Initial values for SyRec variables set!')
 
     def user_func_1(self):
         """
@@ -427,6 +416,10 @@ class TandaPaySimulator(object):
                 self.set_subgroup_status(i, 'invalid')
                 self.set_invalid_refund_available(i, self.get_total_payment_specific_user(i))  # UsRec 10 = UsRec 11
                 self.sy_rec_p[6].value += 1  # Increase invalid members count
+        # PATH 1
+        for k in range(1, 20):
+            self.sy_rec_f[k].value = self.sy_rec_p[k].value
+            self.sy_rec_r[k].value = self.sy_rec_p[k].value
 
     def sys_func_6(self):
         """"
