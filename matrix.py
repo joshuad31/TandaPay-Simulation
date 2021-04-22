dc91e97fc3cfc45e8b2559a542923daa56727989
RPi Guru <rpiguru@techie.com>
2021-04-22 11:00:41 +0800

    Fixed #15

--- matrix.py
+++ matrix.py
@@ -42,15 +42,15 @@ def create_matrix_result(dataset):
         r = sim.start_simulation()
         collapsed = 1 if r[1] / r[0] < .5 else 0
         results.append({'ev': ev, 'pv': pv, 'c': collapsed, 'result': r})
-        sh_map.cell(i + 3, 1).value = i + 1
-        sh_map.cell(i + 3, 2 + ev4_list.index(ev[3])).value = collapsed
-        sh_map.cell(i + 3, 7 + pv4_list.index(pv[3])).value = collapsed
-        sh_map.cell(i + 3, 11 + pv5_list.index(pv[4])).value = collapsed
-        sh_map.cell(i + 3, 15 + ev3_list.index(ev[2])).value = collapsed
-        sh_map.cell(i + 3, 18 + ev1_list.index(ev[0])).value = collapsed
-        sh_map.cell(i + 3, 22 + ev5_list.index(ev[4])).value = collapsed
-        sh_map.cell(i + 3, 24 + ev6_list.index(ev[5])).value = collapsed
-        sh_map.cell(i + 3, 26 + ev7_list.index(ev[6])).value = collapsed
+        sh_map.cell(i + 5, 1).value = i + 1
+        sh_map.cell(i + 5, 2 + ev4_list.index(ev[3])).value = collapsed
+        sh_map.cell(i + 5, 7 + pv4_list.index(pv[3])).value = collapsed
+        sh_map.cell(i + 5, 11 + pv5_list.index(pv[4])).value = collapsed
+        sh_map.cell(i + 5, 15 + ev3_list.index(ev[2])).value = collapsed
+        sh_map.cell(i + 5, 18 + ev1_list.index(ev[0])).value = collapsed
+        sh_map.cell(i + 5, 22 + ev5_list.index(ev[4])).value = collapsed
+        sh_map.cell(i + 5, 24 + ev6_list.index(ev[5])).value = collapsed
+        sh_map.cell(i + 5, 26 + ev7_list.index(ev[6])).value = collapsed
 
         sh_log.cell(i + 2, 1).value = i + 1
         for j in range(7):
@@ -62,24 +62,33 @@ def create_matrix_result(dataset):
         for j, v in enumerate(r):
             sh_log.cell(i + 2, 15 + j).value = v
 
-    total = len(dataset)
-
     for i, ev4 in enumerate(ev4_list):
-        sh_map.cell(2, 2 + i).value = round(len([r for r in results if r['c'] and r['ev'][3] == ev4]) / total * 100, 2)
+        sh_map.cell(2, 2 + i).value = len([r for r in results if r['ev'][3] == ev4])
+        sh_map.cell(3, 2 + i).value = len([r for r in results if r['ev'][3] == ev4 and r['c']])
     for i, pv4 in enumerate(pv4_list):
-        sh_map.cell(2, 7 + i).value = round(len([r for r in results if r['c'] and r['pv'][3] == pv4]) / total * 100, 2)
+        sh_map.cell(2, 7 + i).value = len([r for r in results if r['pv'][3] == pv4])
+        sh_map.cell(3, 7 + i).value = len([r for r in results if r['pv'][3] == pv4 and r['c']])
     for i, pv5 in enumerate(pv5_list):
-        sh_map.cell(2, 11 + i).value = round(len([r for r in results if r['c'] and r['pv'][4] == pv5]) / total * 100, 2)
+        sh_map.cell(2, 11 + i).value = len([r for r in results if r['pv'][4] == pv5])
+        sh_map.cell(3, 11 + i).value = len([r for r in results if r['pv'][4] == pv5 and r['c']])
     for i, ev3 in enumerate(ev3_list):
-        sh_map.cell(2, 15 + i).value = round(len([r for r in results if r['c'] and r['ev'][2] == ev3]) / total * 100, 2)
+        sh_map.cell(2, 15 + i).value = len([r for r in results if r['ev'][2] == ev3])
+        sh_map.cell(3, 15 + i).value = len([r for r in results if r['ev'][2] == ev3 and r['c']])
     for i, ev1 in enumerate(ev1_list):
-        sh_map.cell(2, 18 + i).value = round(len([r for r in results if r['c'] and r['ev'][0] == ev1]) / total * 100, 2)
+        sh_map.cell(2, 18 + i).value = len([r for r in results if r['ev'][0] == ev1])
+        sh_map.cell(3, 18 + i).value = len([r for r in results if r['ev'][0] == ev1 and r['c']])
     for i, ev5 in enumerate(ev5_list):
-        sh_map.cell(2, 22 + i).value = round(len([r for r in results if r['c'] and r['ev'][4] == ev5]) / total * 100, 2)
+        sh_map.cell(2, 22 + i).value = len([r for r in results if r['ev'][4] == ev5])
+        sh_map.cell(3, 22 + i).value = len([r for r in results if r['ev'][4] == ev5 and r['c']])
     for i, ev6 in enumerate(ev6_list):
-        sh_map.cell(2, 24 + i).value = round(len([r for r in results if r['c'] and r['ev'][5] == ev6]) / total * 100, 2)
+        sh_map.cell(2, 24 + i).value = len([r for r in results if r['ev'][5] == ev6])
+        sh_map.cell(3, 24 + i).value = len([r for r in results if r['ev'][5] == ev6 and r['c']])
     for i, ev7 in enumerate(ev7_list):
-        sh_map.cell(2, 26 + i).value = round(len([r for r in results if r['c'] and r['ev'][6] == ev7]) / total * 100, 2)
+        sh_map.cell(2, 26 + i).value = len([r for r in results if r['ev'][6] == ev7])
+        sh_map.cell(3, 26 + i).value = len([r for r in results if r['ev'][6] == ev7 and r['c']])
+
+    for i in range(26):
+        sh_map.cell(4, 2 + i).value = round(sh_map.cell(3, 2 + i).value / sh_map.cell(2, 2 + i).value * 100, 2)
 
     result_file = os.path.join(_cur_dir, 'result', f"Matrix_{datetime.now().strftime('%m_%d_%Y__%H_%M_%S')}.xlsx")
     workbook.save(result_file)
