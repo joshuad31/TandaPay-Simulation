b5f1ff6bf4f5bed7cd4b7ea9048bc8317f64843d
RPi Guru <rpiguru@techie.com>
2023-02-21 00:09:19 +0800

    Updated logger.

--- utils/logger.py
+++ utils/logger.py
@@ -1,24 +1,49 @@
 import os
 import logging
+import sys
 from logging.handlers import RotatingFileHandler
-from logging import Formatter
-from logging import StreamHandler
-
 from settings import ROOT_DIR
 
 
-log_fname = os.path.join(ROOT_DIR, "tandapay_simulation.log")
-log_formatter = Formatter(fmt="%(asctime)s :: %(name)s :: %(levelname)s :: %(message)s")
-file_handler = RotatingFileHandler(filename=log_fname, mode='a', maxBytes=5 * 1024 * 1024, backupCount=5,
-                                   encoding='utf8', delay=False)
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(fmt=log_formatter)
+LOG_FILE = os.path.join(ROOT_DIR, "tandapay_simulation.log")
 
-console_handler = StreamHandler()
-console_handler.setFormatter(fmt=log_formatter)
-console_handler.setLevel(logging.DEBUG)
 
-logger = logging.getLogger('TPS')
-logger.setLevel(logging.DEBUG)
-logger.addHandler(file_handler)
+class CustomFormatter(logging.Formatter):
+    """Logging Formatter to add colors and count warning / errors"""
+
+    grey = "\x1b[38;21m"
+    yellow = "\x1b[33;21m"
+    red = "\x1b[31;21m"
+    bold_red = "\x1b[31a;1m"
+    reset = "\x1b[0m"
+    _format = "%(asctime)s:: %(name)s :: %(levelname)-4s :: %(message)s [%(filename)s:%(lineno)d]"
+
+    FORMATS = {
+        logging.DEBUG: grey + _format + reset,
+        logging.INFO: grey + _format + reset,
+        logging.WARNING: yellow + _format + reset,
+        logging.ERROR: red + _format + reset,
+        logging.CRITICAL: bold_red + _format + reset,
+    }
+
+    def format(self, record):  # type: ignore
+        log_fmt = self.FORMATS.get(record.levelno)
+        _formatter = logging.Formatter(log_fmt)
+        return _formatter.format(record)
+
+
+logger = logging.getLogger("TPS")
+logger.setLevel(logging.DEBUG)  # better to have too much log than not enough
+logger.handlers = []
+formatter = CustomFormatter()
+
+# Add console handler
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
+
+# Add file handlers
+f_info_handler = RotatingFileHandler(LOG_FILE, maxBytes=5 * 1024 * 1024, backupCount=4)
+f_info_handler.setLevel(logging.DEBUG)
+f_info_handler.setFormatter(formatter)
+logger.addHandler(f_info_handler)
